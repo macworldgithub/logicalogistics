0424eefdc04126be5bc286763e2e268a507d1580
Ayesha Fahim <ayesha.fahim02@gmail.com>
2025-04-09 03:49:46 +0500

    done

--- src/app/container_movement/page.tsx
+++ src/app/container_movement/page.tsx
@@ -3,16 +3,28 @@
 import React from 'react';
 import PageWrapper from '../../../utils/PageWrapper';
 import SidebarServices from '../../../common/services';
+import FCLvsLCL from './diagram1';
+import FCLvsLCLDiagram2 from './diagram2';
+
 const Page = () => {
   return (
     <PageWrapper>
-        <div className='w-[99vw]'>
-        <div className='w-[80%] mx-auto'>
-      <SidebarServices/>
-      </div>
+      <div className="w-full px-4 md:px-8 lg:px-12 max-w-7xl mx-auto">
+        <div className="flex flex-col lg:flex-row gap-10">
+          {/* Sidebar */}
+          <div className="w-full lg:w-1/4">
+            <SidebarServices />
+          </div>
+
+          {/* Main Content */}
+          <div className="w-full lg:w-3/4">
+            <FCLvsLCL />
+            <FCLvsLCLDiagram2 />
+          </div>
+        </div>
       </div>
     </PageWrapper>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
