64a3b376382894cf5ad8ffcc7ea65b69d36e846d
Ayesha Fahim <ayesha.fahim02@gmail.com>
2025-04-09 12:09:05 +0500

    done

---
+++ src/app/communityPage/page.tsx
@@ -0,0 +1,13 @@
+import CommunitySection from './our_community';
+
+export default function Home() {
+  return (
+    <div className='w-[99vw]'>
+        <div className='w-[80%] mx-auto'>
+    <main>
+      <CommunitySection />
+    </main>
+    </div>
+    </div>
+  );
+}
