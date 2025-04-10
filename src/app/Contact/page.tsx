09c6d979cdb456cc1f8c68b3b3fc80960b26fcac
shweeta <shweeta@macworldtechnology.com>
2025-04-10 15:18:30 +0500

    removed

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { MapPin, Mail, Phone } from "lucide-react";
 import Navbar from "../../../Components/Navbar";
-import Footer from "../../../Components/Footer";
+// import Footer from "../../../Components/Footer";
 import cartage from "../assets/cartage.png";
 
 export default function Contact() {
