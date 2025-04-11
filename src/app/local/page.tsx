0de3144f9dcfb2627a05fdfc8a08d33654a2936a
Areeba khan <arkhan.2003k@gmail.com>
2025-04-11 02:23:32 -0700

    local done

--- src/app/local/page.tsx
+++ src/app/local/page.tsx
@@ -1,32 +1,62 @@
+// "use client";
+
+// import React from "react";
+// import PageWrapper from "../../../utils/PageWrapper";
+// import SidebarServices from "../../../common/services";
+// import Image from "next/image";
+// import State from "../inter/state";
+// const Page = () => {
+//   return (
+//     <PageWrapper>
+//       <div className="w-[99vw] h-[50vw]  ">
+//         <div className="relative w-full h-1/2">
+//           <Image
+//             src="/home/BANNER.svg"
+//             alt="Home Banner"
+//             fill
+//             className="object-cover"
+//           />
+//         </div>
+//       </div>
+//       <div className="w-[99vw]">
+//         <div className="w-[80%] mx-auto">
+//           <div className="flex flex-row">
+//             <SidebarServices />
+//             <div className="-mt-5">
+//               <State />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </PageWrapper>
+//   );
+// };
+
+// export default Page;
+
 "use client";
 
-import React from "react";
-import PageWrapper from "../../../utils/PageWrapper";
-import SidebarServices from "../../../common/services";
-import Service from "../local/service";
-import Image from "next/image";
 import PageHeader from "../../../common/PageHeader";
+import SidebarServices from "../../../common/services";
+import PageWrapper from "../../../utils/PageWrapper";
+import Service from "../local/service";
 const Page = () => {
   return (
     <PageWrapper>
-      <PageHeader title="Local" breadcrumb="Local" />
-      <div className="w-[99vw] h-[50vw]  ">
-        <div className="relative w-full h-1/2">
-          <Image
-            src="/home/BANNER.svg"
-            alt="Home Banner"
-            fill
-            className="object-cover"
-          />
-        </div>
-      </div>
-      <div className="w-[99vw]">
-        <div className="w-[80%] mx-auto">
-          <div className="flex flex-row">
+      <PageHeader
+        title="Local"
+        breadcrumb="Local"
+      />
+      <div className="w-full px-4 md:px-8 lg:px-12 max-w-7xl mx-auto">
+        <div className="flex flex-col sm:flex-row gap-10">
+          {/* Sidebar */}
+          <div className="max-sm:w-full w-1/4 mt-10">
             <SidebarServices />
-            <div className="-mt-5">
-              <Service />
-            </div>
+          </div>
+
+          {/* Main Content */}
+          <div className="max-sm:w-full w-3/4">
+            <Service />
           </div>
         </div>
       </div>
